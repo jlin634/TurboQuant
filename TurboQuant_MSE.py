443ff3f1ec0ee2e6e49c4ee6deaf0111df5eb4ad
Jeffrey Lin <jblin2018@gmail.com>
2026-05-24 16:51:44 -0400

    "Test"

--- TurboQuant_MSE.py
+++ TurboQuant_MSE.py
@@ -1,1 +1,7 @@
-## from Luke
+# TurboQuant MSE
+# Author: Jeffrey Lin, Luke Ito, Claudia Huynh 
+# Date: 05/24/2026
+# Program Purpose: Implementation of Google Research Vector Quantization Algorithm
+
+def randomize_matrix(): 
+    print("hello world")
